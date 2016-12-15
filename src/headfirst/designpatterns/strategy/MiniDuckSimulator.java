48d2bc5914d5e5635462e2a09f27a7ae1f038380
omottec <omottec@gmail.com>
2016-12-15 15:15:31 +0800

    modify strategy

--- src/headfirst/designpatterns/strategy/MiniDuckSimulator.java
+++ src/headfirst/designpatterns/strategy/MiniDuckSimulator.java
@@ -5,10 +5,10 @@ public class MiniDuckSimulator {
 	public static void main(String[] args) {
  
 		MallardDuck	mallard = new MallardDuck();
-		RubberDuck	rubberDuckie = new RubberDuck();
-		DecoyDuck	decoy = new DecoyDuck();
+		RubberDuck rubberDuckie = new RubberDuck();
+		DecoyDuck decoy = new DecoyDuck();
  
-		Duck	 model = new ModelDuck();
+		Duck model = new ModelDuck();
 
 		mallard.performQuack();
 		rubberDuckie.performQuack();
